755458ca2491d078087a9706bad15c6c5318a8b1
mfaisalkhatri <mohammadfaisalkhatri@gmail.com>
2023-02-06 17:29:27 +0530

    refactored Android tests and added readme

--- src/test/java/io/github/mfaisalkhatri/tests/AndroidTests.java
+++ src/test/java/io/github/mfaisalkhatri/tests/AndroidTests.java
@@ -6,7 +6,6 @@ import static org.testng.Assert.assertTrue;
 import io.github.mfaisalkhatri.pages.android.HomePage;
 import io.github.mfaisalkhatri.pages.android.LoginPage;
 import io.github.mfaisalkhatri.pages.android.SearchFlightPage;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 /**
@@ -15,15 +14,9 @@ import org.testng.annotations.Test;
  **/
 public class AndroidTests extends BaseTest {
 
-    private HomePage homePage;
-
-    @BeforeClass
-    public void setup () {
-        homePage = new HomePage (androidDriverManager);
-    }
-
     @Test
     public void testEndUserLicense () {
+        HomePage homePage = new HomePage (androidDriverManager);
         String endUserLicenseText = "Terms and Conditions relating to the pCloudy Application  General By clicking on the \"Accept\" button, you are agreeing to be bound by these Terms and Conditions. Please review them carefully before giving your acceptance. This application (\"pCloudy Application\") is owned and operated by pCloudy.com, whose registered office is at #hird Floor, Nishvi Building, 2729, 16th Cross Rd, PWD Quarters, 1st Sector, HSR Layout, Bengaluru, Karnataka 560102. \"We\", \"Our\" and \"Us\" refers to pCloudy.  Data Protection Any personal information you supply to Us when you use the pcloudy Application will be used in accordance with Our Privacy Policy which can be viewed at http://www.pcloudy.com The plcoudy test Application allows you to access certain functionality available on the website. Such access will be governed by the pcloudy website user agreement which can be viewed at http://www.pcloudy.com.";
         assertEquals (homePage.endUserLicenseText (), endUserLicenseText);
         homePage.acceptLicense ();
